ec54ef5696277bdde828b6c6414b1bf4eb142c8b
Stellin-15 <123811154+Stellin-15@users.noreply.github.com>
2024-10-12 00:17:36 +0400

    second commit
    backend for this is done now going to build the sign up and login part of it

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const connection = require('./db.js');
+const userRoutes = require('./routes/users');
+const authRoutes = require('./routes/auth');
+
 
 // database bro 
 connection();
@@ -12,7 +15,9 @@ connection();
 app.use(express.json())
 app.use(cors());
 
-
+// routes
+app.use("/api/users",userRoutes);
+app.use("/api/auth",authRoutes);
 
 const port = process.env.PORT || 8080;
 app.listen(port,()=>console.log('Listening on port ${port}...'))
--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const jwt = require('jsonwebtoken');
 const Joi = require('joi');
-
+const passwordComplexity = require('joi-password-complexity');
 
 
 const userSchema = new mongoose.Schema({
@@ -20,7 +20,18 @@ userSchema.methods.generateAuthToken = function(){
 
 const User = mongoose.model('user',userSchema);
 
+const validate = (data) =>{
+    const schema = Joi.object({
+        firstName:Joi.string().required().label("First Name"),
+        lastName:Joi.string().required().label("Last Name"),
+        email:Joi.string().email().required().label("Email"),
+        password: passwordComplexity().required().label("Password")
+    });
+    return schema.validate(data)
+    
+};
 
+module.exports = {User,validate};
 
 
 
