b3bc7cd3374ed4f8ccb0d468af1af8a1a8b1e215
Sreenikethan I <29738979+SreenikethanI@users.noreply.github.com>
2024-10-27 23:58:54 +0400

    server: database connection error handling

--- server/db.js
+++ server/db.js
@@ -7,10 +7,16 @@ const connectionParams = {
     useUnifiedTopology: true,
 };
 
-try {
-    module.exports = mongoose.connect(process.env.DB_URL, connectionParams);
-    console.log("connected to database successfully!! Let's goooo...");
-} catch(error) {
-    console.log(error);
-    console.log("fuck bro, something went wrong try again!")
-}
+/** @type {mongoose.Mongoose | null} */
+var connection = null;
+await mongoose
+    .connect(process.env.DB_URL || "")
+    .then((conn) => {
+        connection = conn;
+        console.log("Connected to database successfully.");
+    })
+    .catch((error) => {
+        console.error("Error connecting to database. Reason:", error);
+    });
+
+export default connection;
