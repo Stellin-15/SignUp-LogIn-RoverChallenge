2e2ab3d5cd3aecaa061f59d0c61dec5175c78999
Sreenikethan I <29738979+SreenikethanI@users.noreply.github.com>
2024-10-28 11:04:03 +0400

    client: now runs

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import {BrowserRouter} from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+root.render(
+    <StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </StrictMode>
 );
-
-
